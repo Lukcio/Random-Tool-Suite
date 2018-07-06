03ab7fa86f5f329706588606aa2e5c779895b524
Lucas Leschynski <lucasleschynski@lucass-macbook-pro.local>
2018-07-06 12:02:06 -0500

    Created basic UI framework - 4 VCs and return functionality working

---
+++ Random App Suite/CoinViewController.swift
@@ -0,0 +1,41 @@
+//
+//  CoinViewController.swift
+//  Random App Suite
+//
+//  Created by Lucas Leschynski on 7/6/18.
+//  Copyright © 2018 Lucas Leschynski. All rights reserved.
+//
+
+import UIKit
+
+class CoinViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func unwindToInitialViewController(segue: UIStoryboardSegue) {
+    }
+    
+    @IBAction func flipButton(_ sender: UIButton) {
+        print("this")
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Random App Suite/RandomNumberViewController.swift
@@ -0,0 +1,38 @@
+//
+//  RandomNumberViewController.swift
+//  Random App Suite
+//
+//  Created by Lucas Leschynski on 7/6/18.
+//  Copyright © 2018 Lucas Leschynski. All rights reserved.
+//
+
+import UIKit
+
+class RandomNumberViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func unwindToInitialViewController(segue: UIStoryboardSegue) {
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

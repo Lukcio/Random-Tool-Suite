18773a3f4efb4d013ee4c117833ac21eeff52d24
Lucas Leschynski <lucasleschynski@lucass-macbook-pro.local>
2018-07-06 16:14:04 -0500

    Began working on the Random Name Generator section

--- Random App Suite/RandomNameViewController.swift
+++ Random App Suite/RandomNameViewController.swift
@@ -9,10 +9,29 @@
 import UIKit
 
 class RandomNameViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var nameOriginPicker: UIPickerView!
+    
+    //NAME ARRAYS
+    let fantasyNamesArray = ["this", "that"]
+    let englishNamesArray = ["Oliver","George","Harry","Jack","Jacob","Noah","Charlie","Thomas","Oscar","William","James",
+    "Henry","Leo","Alfie","Joshua","Freddie","Archie","Ethan","Isaac","Alexander","Joseph","Edward","Samuel","Max","Daniel",
+    "Arthur","Lucas","Logan","Theo","Harrison","Benjamin","Mason","Sebastian","Finley","Adam","Dylan","Zachary","Riley","Teddy",
+    "Theodore","David","Toby","Jake","Louie","Elijah","Reuben","Arlo","Hugo","Luca","Jaxon","Matthew","Harvey","Reggie","Michael",
+    "Harley","Jude","Albert","Tommy","Luke","Stanley","Jenson","Frankie","Jayden","Gabriel","Elliot","Ronnie","Charles","Louis",
+    "Elliott","Frederick","Nathan","Lewis","Blake","Ollie","Ryan","Tyler","Jackson","Dexter","Alex","Austin","Albie","Caleb",
+    "Carter","Bobby","Ezra","Ellis","Leon","Roman","Aaron","Liam","Jesse","Jasper","Felix","Jamie"]
+    let numberOfNamesArray = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10",]
+    var selectedNumberOfNames: Int = 0
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return numberOfNamesArray.count
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        print(englishNamesArray)
         // Do any additional setup after loading the view.
     }
 
